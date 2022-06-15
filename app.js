1d3c343de5fb0b76a8c67cf554f02771d78ab3d7
Jim Ray <jray@slack-corp.com>
2022-06-14 22:11:08 -0400

    Generate the shortcute modal

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // use the values stored in .env
 require('dotenv').config();
-import { Sequelize, Model, DataTypes } from 'sequelize';
+const { Sequelize, DataTypes } = require('sequelize');
 
 const { App } = require('@slack/bolt');
+
+//intialize the Bolt app
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
+});
 
 // set up the data model
 const sequelize = new Sequelize('sqlite::memory:');
@@ -11,11 +18,69 @@ const Todo = sequelize.define('Todo', {
   due: DataTypes.DATE,
 });
 
-//intialize the Bolt app
-const app = new App({
-  token: process.env.SLACK_BOT_TOKEN,
-  appToken: process.env.SLACK_APP_TOKEN,
-  socketMode: true,
+// Respond to the shortcut to create a modal for defining the todo
+app.shortcut('new_todo', async ({ shortcut, ack, client, logger }) => {
+  try {
+    // Acknowledge shortcut request
+    await ack();
+
+    // Call the views.open method to create a modal
+    const result = await client.views.open({
+      trigger_id: shortcut.trigger_id,
+      view: {
+        type: "modal",
+        title: {
+          type: "plain_text",
+          text: "New Todo"
+        },
+        submit: {
+          type: "plain_text",
+          text: "Submit",
+          emoji: true
+        },
+        close: {
+          type: "plain_text",
+          text: "Close"
+        },
+        blocks: [
+          {
+            type: "input",
+            element: {
+              type: "plain_text_input",
+              action_id: "plain_text_input-action"
+            },
+            label: {
+              type: "plain_text",
+              text: "What do you want to do?",
+              emoji: true
+            }
+          },
+          {
+            type: "input",
+            element: {
+              type: "datepicker",
+              placeholder: {
+                type: "plain_text",
+                text: "Select a date",
+                emoji: true
+              },
+              action_id: "datepicker-action"
+            },
+            label: {
+              type: "plain_text",
+              text: "When do you want to do it?",
+              emoji: true
+            }
+          }
+        ]
+      }
+    });
+
+    logger.info(result);
+  }
+  catch (error) {
+    logger.error(error);
+  }
 });
 
 (async () => {
