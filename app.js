d8b55cba70596d5daa65f352ac895abc32d5a631
Jim Ray <jray@slack-corp.com>
2022-06-14 22:16:48 -0400

    added the callback id

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.shortcut('new_todo', async ({ shortcut, ack, client, logger }) => {
     // Call the views.open method to create a modal
     const result = await client.views.open({
       trigger_id: shortcut.trigger_id,
+      callback_id: 'todo_view',
       view: {
         type: "modal",
         title: {
